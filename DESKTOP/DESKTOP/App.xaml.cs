f49ef284eea6733c4a0503e3b876a0d3c9a2dcdb
NikoirDevelopment <hlebushek0612@ya.ru>
2024-11-27 23:43:50 +0900

    v0.0.4
    * Создание пустого репозитория:
        - Создание общей папки "res"
        - Создание общей папки "data"
        - Создана папка "tests" для будущих UnitTest
        - Создана новая директория ASP.NET, для разработки веб-приложения
        - Создана новая директория .NET MAUI, для разработки Desktop приложения под MacOS и Windows
    
    * Создание пространства для СУБД PostgreSQL:
        - Добавлены отформатированные данные к 3NF в формате xlsx
        - Созданы конвертированные данные из xlsx в csv utf-8
        - Создан SQL запрос для создания таблицы в СУБД

---
+++ DESKTOP/DESKTOP/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DESKTOP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
